071da101f7df91759027d0b4fa712f7e0dd8457c
MuhammadFandiH <60420960+MuhammadFandiH@users.noreply.github.com>
2020-01-29 13:38:38 +0700

    Add files via upload

---
+++ welcome.blade.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Perkenalan</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #purple;
+                color: #darkblue;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 80px;
+            }
+
+            .links > a {
+                color: #green;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>  
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    <b>Muhammad Fandi H</b>
+                </div>
+
+                <div class="links">
+                    <a href="https://laravel.com/docs"></a>
+                    <a href="https://laracasts.com">Mahasiwa</a>
+                    <a href="https://laravel-news.com">Teknologi Informasi</a>
+                    <a href="https://blog.laravel.com">Vokasi</a>
+                    <a href="https://nova.laravel.com">Universitas</a>
+                    <a href="https://forge.laravel.com">Brawijaya</a>
+                    <a href="https://vapor.laravel.com">Kota Malang</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
